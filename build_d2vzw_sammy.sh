8bf80ea867c900a910ab2065b460028650193339
ktoonsez <ktoonsez@gmail.com>
2014-05-10 10:20:52 -0700

    Fixup defconfig for root access, build scripts and some gitignores

--- build_d2vzw_sammy.sh
+++ build_d2vzw_sammy.sh
@@ -38,7 +38,7 @@ rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print)
 rm -rf $(find $INITRAMFS_DEST -name .git -print)
 
 echo "Make the kernel"
-make KT747_d2vzw_defconfig
+make KT747_defconfig VARIANT_DEFCONFIG=msm8960_m2_vzw_defconfig
 
 HOST_CHECK=`uname -n`
 if [ $HOST_CHECK = 'ktoonsez-VirtualBox' ]; then
